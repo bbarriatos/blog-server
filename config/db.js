b133fdb071c86414fafb41c68f2541b490e57cae
bbarriatos <bbarriatos@gmail.com>
2021-06-13 03:53:00 +0800

    main files update for db security

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@blog-todo.yypn0.mongodb.net/blog_server`;
+const db = `mongodb+srv://root:dE0U3Rqivo24MEOV@blog-todo.yypn0.mongodb.net/blog_server`;
 
 const connectDB = () => {
   try {
