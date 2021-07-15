5240b0bab80e39cd1dfb87f7e0d3ba6e37ea1d8c
bbarriatos <bbarriatos@gmail.com>
2021-07-16 05:47:11 +0800

    Done on the login page

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
+const path = require('path');
+const handlebars = require('express-handlebars');
+const orderid = require('order-id')('mysecret');
+const id = orderid.generate();
 
 require('dotenv').config();
 
-app.use(express.json({ extended: false }));
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'index',
+    helpers: {
+      copyrightYear: function () {
+        return new Date().getFullYear();
+      },
+    },
+    partialsDir: __dirname + '/views/partials/',
+  })
+);
 
 connectDB();
+
+app.use(express.static('public'));
+
+// app.use('/', require('./routes/User'));
 
 app.use('/api/users', require('./routes/User'));
 app.use('/api/auth', require('./routes/Auth'));
@@ -14,6 +36,18 @@ app.use('/api/status', require('./routes/Status'));
 app.use('/api/task', require('./routes/Task'));
 app.use('/api/category', require('./routes/Category'));
 app.use('/api/posts', require('./routes/Posts'));
+
+const defaultPageConfig = {
+  title: 'Login | Bon Blog Site',
+  listExists: true,
+};
+
+app.get('/', (req, res) => {
+  res.render('main', {
+    ...defaultPageConfig,
+    bodyClass: `bg-gradient-primary`,
+  });
+});
 
 app.listen(5000, () => {
   console.log(`Listening on port 5000`);
