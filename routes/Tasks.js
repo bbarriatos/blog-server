e1ec61e19f92678a9c0dacddd149886876899477
bbarriatos <bbarriatos@gmail.com>
2021-10-29 02:53:34 +0800

    Done on the update and delete feature of task

--- routes/Tasks.js
+++ routes/Tasks.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
 const Tasks = require('../models/Task');
-const User = require('../models/User');
+const Category = require('../models/Category');
+const Status = require('../models/Status');
 const router = express.Router();
 const { pageConfig } = require('../config/defaultPageConfig');
 const fs = require('fs');
@@ -37,12 +38,18 @@ router.get('/addTask', async (req, res) => {
   }
 });
 
-router.get('/editTask', async (req, res) => {
+router.get('/:taskId', async (req, res) => {
   try {
-    res.render('home/tasks/editTask', {
-      ...pageConfig,
-      title: 'Update Task | Bon Blog Site',
-      bodyClass: `bg-gradient-primary`,
+    const status = await Status.find();
+
+    Tasks.findOne({ _id: req.params.taskId }).then((taskData) => {
+      res.render('home/tasks/editTask', {
+        ...pageConfig,
+        title: 'Update Task | Bon Blog Site',
+        bodyClass: `bg-gradient-primary`,
+        task: taskData,
+        status: status.filter((stat) => stat.status_category === 'Tasks'),
+      });
     });
   } catch (error) {
     res.status(500).json({ message: 'Server Error' });
@@ -98,25 +105,25 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const err = validationResult(req);
-  const { title, content, date, due, status } = req.body;
+  // const err = validationResult(req);
+  const { title, content, daterange, status } = req.body;
 
-  if (!err.isEmpty()) {
-    return res.status(400).json({ errors: err.array() });
-  }
+  // if (!err.isEmpty()) {
+  //   return res.status(400).json({ errors: err.array() });
+  // }
 
   try {
-    const task = await Task.findById(req.params.id);
+    const task = await Tasks.findById(req.params.id);
 
     task.task_title = title;
     task.task_content = content;
-    task.task_date = date;
-    task.task_due = due;
+    task.task_due = daterange;
     task.status = status;
 
-    await task.save();
-
-    res.json(task);
+    await task.save().then(() => {
+      req.flash('success_message', 'Task was updated successfully');
+      res.redirect('/tasks');
+    });
   } catch (error) {
     res.status(500).json({ message: 'Server Error' });
   }
@@ -124,11 +131,12 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    const task_due = await Task.findByIdAndDelete(req.params.id);
+    const task_due = await Tasks.findByIdAndDelete(req.params.id);
 
-    await task_due.remove();
-
-    res.json('Task Deleted');
+    await task_due.remove().then((savedPost) => {
+      req.flash('success_message', 'Task deleted successfully');
+      res.redirect('/tasks');
+    });
   } catch (error) {
     res.status(500).send('Server Error');
   }
